bc1ae280ade84fa7e77129233253924a0b2727ad
Rizal Ilham <rzlilhm09@gmail.com>
2021-07-08 17:29:20 +0700

    Appointment and Docter db models initialized

---
+++ models/appointment.py
@@ -0,0 +1,17 @@
+from functools import partialmethod
+from app import app, db
+
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+    appointment_id = db.Column(db.Integer, primary_key=True)
+    appointment_title = db.Column(db.String(100), nullable=False)
+    appointment_desc = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    docter_id = db.Column(db.Integer, db.ForeignKey('docters.docter_id'))
+    
+
+    def __init__(self, appointment_title):
+        self.appointment_title = appointment_title
+
+    def __repr__(self):
+        return "<Appointment title: {}>".format(self.appointment_title)
---
+++ models/docter.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import backref
+from app import app, db
+
+class Docter(db.Model):
+    __tablename__ = 'docters'
+    docter_id = db.Column( db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    appointments = db.relationship('Appointment', backref='docter', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return "<Docter name: {}>".format(self.name)
