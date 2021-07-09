85f4873c74e3cf659e0590152b2749fedaa84de5
Rizal Ilham <rzlilhm09@gmail.com>
2021-07-09 20:09:24 +0700

    Update appointment.py __init__

--- hospital/models/appointment.py
+++ hospital/models/appointment.py
@@ -1,17 +1,21 @@
 from functools import partialmethod
+
+from sqlalchemy.sql.operators import nullslast_op
 from hospital import db
 
 class Appointment(db.Model):
     __tablename__ = 'appointments'
     appointment_id = db.Column(db.Integer, primary_key=True)
     appointment_title = db.Column(db.String(100), nullable=False)
-    appointment_desc = db.Column(db.Text)
+    appointment_desc = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     docter_id = db.Column(db.Integer, db.ForeignKey('docters.docter_id'))
     
 
-    def __init__(self, appointment_title):
+    def __init__(self, appointment_title, appointment_desc, docter_id):
         self.appointment_title = appointment_title
+        self.appointment_desc = appointment_desc
+        self.docter_id = docter_id
 
     def __repr__(self):
-        return "<Appointment title: {}>".format(self.appointment_title)
+        return self.appointment_title
