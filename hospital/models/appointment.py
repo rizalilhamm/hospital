a69afbffb6561ccab1b5954e70bdf95ad3083c4f
Rizal Ilham <rzlilhm09@gmail.com>
2021-07-10 18:48:30 +0700

    Appointment models add patient_identifier

--- hospital/models/appointment.py
+++ hospital/models/appointment.py
@@ -4,14 +4,22 @@ from functools import partialmethod
 from sqlalchemy.sql.operators import nullslast_op
 from hospital import db
 
+
+patient_identifier = db.Table(
+    'patient_identifier',
+    db.Column('appointment_id', db.Integer, db.ForeignKey('appointments.appointment_id')),
+    db.Column('patient_id', db.Integer, db.ForeignKey('users.user_id'))
+)
+
+
 class Appointment(db.Model):
     __tablename__ = 'appointments'
     appointment_id = db.Column(db.Integer, primary_key=True)
     appointment_title = db.Column(db.String(100), unique=True, nullable=False)
     appointment_desc = db.Column(db.Text, nullable=False)
     max_patient = db.Column(db.Integer, default=10)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     docter_id = db.Column(db.Integer, db.ForeignKey('docters.docter_id'))
+    patients = db.relationship('User', secondary=patient_identifier)
     
 
     def __init__(self, appointment_title, appointment_desc, docter_id, max_patient):
